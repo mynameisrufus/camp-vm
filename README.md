fca1adb220ab205c32e3d761e7510932ec970964
Rufus Post <rufuspost@gmail.com>
2013-09-25 19:09:34 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Campers VM FTW
+
+Features:
+
+* gems
+* npm
+* tweeter
+
+add a new module:
+
+```
+puppet module install ripienaar/concat --modulepath modules
+```
---
+++ manifests/site.pp
@@ -0,0 +1,24 @@
+node default {
+  include git
+  include nginx
+  include postgresql::server
+
+  nginx::resource::upstream { 'puppet_rack_app':
+    ensure  => present,
+    members => [
+      'localhost:3000',
+      'localhost:3001',
+      'localhost:3002',
+    ],
+  }
+
+  nginx::resource::vhost { 'rack.puppetlabs.com':
+    ensure   => present,
+    proxy  => 'http://puppet_rack_app',
+  }
+
+  postgresql::db { 'tweeter':
+    user     => 'tweeter',
+    password => postgresql_password('password', 'tweeter'),
+  }
+} 
